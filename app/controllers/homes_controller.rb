fc56a1c812146fe0e028461f6f6bb93c09f68875
yuukinakamoo <72909522+yuukinakamoo@users.noreply.github.com>
2020-12-20 16:15:17 +0900

    add HelloWorld

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+  def index
+    @greet = "Hello World!!"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get "/homes", to: "homes#index"
+end
